2648a5c9d0adb082dc22d819ef1e7f22af4425c4
MagicTeaMC <oneowo87@gmail.com>
2025-11-23 21:47:39 +0800

    idk

---
+++ vector.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int x1, y1, z1;
+    int x2, y2, z2;
+
+    cin >> x1 >> y1 >> z1;
+    cin >> x2 >> y2 >> z2;
+
+    cout << x2-x1 << ", " << y2-y1 << ", " << z2-z1 << endl;
+
+    return 0;
+}
